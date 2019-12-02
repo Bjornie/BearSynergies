eded268b2fe4e5755a555921dee997fc90ef2934
BjoernTheBurr <lassebjs@gmail.com>
2019-12-02 19:11:34 +0100

    .

---
+++ BearSynergiesMenu.lua
@@ -0,0 +1,26 @@
+local BS = BearSynergies
+local LAM = LibAddonMenu2
+
+function BS.BuildMenu()
+    local panelData = {
+        type = "panel",
+        name = "Bear Synergies",
+        displayName = "Bear Synergies",
+        author = "|c00BFFFBjörnTheBurr|r",
+        version = "0.1",
+        registerForRefresh = true,
+    }
+
+    local optionsTable = {
+        {
+            type = "header",
+            name = "Dragonknight",
+        },
+        {
+            type = "checkbox",
+            name = "Shackle",
+            tooltip = "Disabling this will prevent you from being able to activate the Shackle synergy, provided by the Dragonknight Standard skill.",
+            getFunc = function() return 
+        }
+    }
+end
--- config.lua
+++ config.lua
@@ -1,5 +1,6 @@
 -- Dragonknight
-shackle = 
+shackle = 32905,
+ignite = 32974,
 
 conduit = 23196,
 harvest = 
