b8df3c57ec31fd79e6f92bda6546c1f2977f2100
BjoernTheBurr <lassebjs@gmail.com>
2019-12-06 15:51:23 +0100

    First prototype of working addon

--- BearSynergies.lua
+++ BearSynergies.lua
@@ -1,39 +1,106 @@
 BearSynergies = {
-    Name = "BearSynergies",
-    Version = "0.1",
-    Author = "BjörnTheBurr",
+    name = "BearSynergies",
 
-    defaultSettings = {
+    svVersion = 1,
+
+    default = {
         -- Dragonknight
-        DK_SHACKLE = true,
-        DK_IGNITE = true,
+        ["Shackle"] = true,
+        ["Ignite"] = true,
 
         -- Necromancer
-        NM_GRAVE_ROBBER = true,
+        ["Grave Robber"] = true,
+        ["Pure Agony"] = true,
 
         -- Nightblade
-        NB_HIDDEN_REFRESH = true,
-        NB_SOUL_LEECH = true,
+        ["Hidden Refresh"] = true,
+        ["Soul Leech"] = true,
 
         -- Sorcerer
-        SORC_CHARGED_LIGHTNING = true,
-        SORC_CONDUIT = true,
+        ["Charged Lightning"] = true,
+        ["Conduit"] = true,
+
+        -- Templar
+        ["Blessed Shards"] = true,
+        ["Holy Shards"] = true,
+        ["Supernova"] = true,
+        ["Gravity Crush"] = true,
+        ["Purify"] = true,
+
+        -- Warden
+        ["Harvest"] = true,
+        ["Icy Escape"] = true,
+
+        -- Werewolf
+        ["Feeding Frenzy"] = true,
+
+        -- Undaunted
+        ["Blood Funnel"] = true,
+        ["Blood Feast"] = true,
+        ["Spawn Broodlings"] = true,
+        ["Black Widows"] = true,
+        ["Arachnophobia"] = true,
+        ["Radiate"] = true,
+        ["Bone Wall"] = true,
+        ["Spinal Surge"] = true,
+        ["Combustion"] = true,
+        ["Healing Combustion"] = true,
+
+        -- Arena
+        ["Sigil of Defense"] = true,
+        ["Sigil of Haste"] = true,
+        ["Sigil of Healing"] = true,
+        ["Sigil of Power"] = true,
+        ["Sigil of Resurrection"] = true,
+        ["Sigil of Sustain"] = true,
+
+        -- Trial
+        ["Destructive Outbreak"] = true, -- HRC fossilize break
+        ["Remove Bolt"] = true, -- SO and SS remove pin
+        ["Celestial Purge"] = true, -- MoL lunar phase
+        ["Levitate"] = true, -- HoF 2. boss upstairs
+        ["Power Switch"] = true, --HoF 2. boss upstairs button
+        ["Gateway"] = true, -- CR portal
+        ["Malevolent Core"] = true, -- CR downstairs orbs
+        ["Shed Hoarfrost"] = true, -- CR drop hoarfrost
+        ["Welkynar's Light"] = true, -- CR spears
+        ["Wind of the Welkynar"] = true, -- CR downstairs jump pad
+        ["Time Breach"] = true, -- SS portal
     },
 }
 
 local BS = BearSynergies
-local EM = EVENT_MANAGER
 
+-- This function runs before synergy prompt on screen and determines whether the prompt appears or not
 function BS.PreHook()
     ZO_PreHook(SYNERGY, "OnSynergyAbilityChanged", function()
         local synergyName, iconFilename = GetSynergyInfo()
-
-        if synergyName == 
+        
+        if synergyName and iconFilename then
+            if BS.savedVariables[synergyName] ~= nil then
+                if BS.savedVariables[synergyName] == false then
+                    SYNERGY:SetHidden(true)
+                    return true -- Do not run original code
+                end
+            else
+                -- Always allow unknown synergy
+                SYNERGY:SetHidden(false)
+                return false --Run original code
+            end
+        end
     end)
 end
 
-function BS.init()
-    if addon ~= BS.Name then return end
+function BS.Initialize(event, addonName)
+    BS.savedVariables = ZO_SavedVars:NewAccountWide("BearSynergiesSV", BS.svVersion, nil, BS.default)
+
+    BS.BuildMenu()
+    BS.PreHook()
 end
 
-EM:RegisterForEvent(BearSynergies.Name, EVENT_ADD_ON_LOADED, BearSynergies.init)
+EVENT_MANAGER:RegisterForEvent(BS.name, EVENT_ADD_ON_LOADED, function(_, name)
+    if name == BS.name then
+        BS.Initialize()
+        EVENT_MANAGER:UnregisterForEvent(BS.name, EVENT_ADD_ON_LOADED)
+    end
+end)
--- BearSynergiesMenu.lua
+++ BearSynergiesMenu.lua
@@ -12,17 +12,294 @@ function BS.BuildMenu()
     }
 
     local optionsTable = {
-        -- Dragonknight Settings
-        {
-            type = "header",
-            name = "Dragonknight",
+        -- Class Settings
+        [1] = {
+            type = "submenu",
+            name = "|cFFFACDClass Synergies|r",
+            controls = {
+                -- Dragonknight Settings
+                [1] = {
+                    type = "header",
+                    name = "|cFFFACDDragonknight|",
+                },
+                [2] = {
+                    type = "checkbox",
+                    name = "Shackle",
+                    tooltip = "Disabling this will prevent you from being able to use the Shackle synergy, provided by the Dragonknight Standard skill.",
+                    getFunc = function() return BS.savedVariables["Shackle"] end,
+                    setFunc = function(value) BS.savedVariables["Shackle"] = value end,
+                },
+                [3] = {
+                    type = "checkbox",
+                    name = "Ignite",
+                    tooltip = "Disabling this will prevent you from being able to use the Ignite synergy, provided by the Dark Talons skill.",
+                    getFunc = function() return BS.savedVariables["Ignite"] end,
+                    setFunc = function(value) BS.savedVariables["Ignite"] = value end,
+                },
+
+                -- Necromancer Settings
+                [4] = {
+                    type = "header",
+                    name = "|cFFFACDNecromancer|r",
+                },
+                [5] = {
+                    type = "checkbox",
+                    name = "Grave Robber",
+                    tooltip = "Disabling this will prevent you from being able to use the Grave Robber synergy, provided by the Boneyard skill.",
+                    getFunc = function() return BS.savedVariables["Grave Robber"] end,
+                    setFunc = function(value) BS.savedVariables["Grave Robber"] = value end,
+                },
+                [6] = {
+                    type = "checkbox",
+                    name = "Pure Agony",
+                    tooltip = "Disabling this will prevent you from being able to use the Pure Agony synergy, provided by the Agony Totem skill.",
+                    getFunc = function() return BS.savedVariables["Pure Agony"] end,
+                    setFunc = function(value) BS.savedVariables["Pure Agony"] = value end,
+                },
+
+                -- Nightblade Settings
+                [7] = {
+                    type = "header",
+                    name = "|cFFFACDNightblade|r",
+                },
+                [8] = {
+                    type = "checkbox",
+                    name = "Hidden Refresh",
+                    tooltip = "Disabling this will prevent you from being able to use the Hidden Refresh synergy, provided by the Consuming Darkness skill.",
+                    getFunc = function() return BS.savedVariables["Hidden Refresh"] end,
+                    setFunc = function(value) BS.savedVariables["Hidden Refresh"] = value end,
+                },
+                [9] = {
+                    type = "checkbox",
+                    name = "Soul Leech",
+                    tooltip = "Disabling this will prevent you from being able to use the Soul Leech synergy, provided by the Soul Shred skill.",
+                    getFunc = function() return BS.savedVariables["Soul Leech"] end,
+                    setFunc = function(value) BS.savedVariables["Soul Leech"] = value end,
+                },
+
+                -- Sorcerer Settings
+                [10] = {
+                    type = "header",
+                    name = "|cFFFACDSorcerer|r",
+                },
+                [11] = {
+                    type = "checkbox",
+                    name = "Charged Lightning",
+                    tooltip = "Disabling this will prevent you from being able to use the Charged Lightning synergy, provided by the Summon Storm Atronach skill.",
+                    getFunc = function() return BS.savedVariables["Charged Lightning"] end,
+                    setFunc = function(value) BS.savedVariables["Charged Lightning"] = value end,
+                },
+                [12] = {
+                    type = "checkbox",
+                    name = "Conduit",
+                    tooltip = "Disabling this will prevent you from being able to use the Conduit synergy, provided by the Lightning Splash skill.",
+                    getFunc = function() return BS.savedVariables["Conduit"] end,
+                    setFunc = function(value) BS.savedVariables["Conduit"] = value end,
+                },
+
+                -- Templar Settings
+                [13] = {
+                    type = "header",
+                    name = "|cFFFACDTemplar|r",
+                },
+                [14] = {
+                    type = "checkbox",
+                    name = "Shards",
+                    tooltip = "Disabling this will prevent you from being able to use the Blessed Shards and Holy Shards synergies, provided by the Spear Shards and morphs skills.",
+                    getFunc = function() return BS.savedVariables["Blessed Shards"] end,
+                    setFunc = function(value) BS.savedVariables["Blessed Shards"] = value end,
+                    getFunc = function() return BS.savedVariables["Holy Shards"] end,
+                    setFunc = function(value) BS.savedVariables["Holy Shards"] = value end,
+                },
+                [15] = {
+                    type = "checkbox",
+                    name = "Nova",
+                    tooltip = "Disabling this will prevent you from being able to use the Supernova and Gravity Crush synergies, provided by the Nova and morphs skills.",
+                    getFunc = function() return BS.savedVariables["Supernova"] end,
+                    setFunc = function(value) BS.savedVariables["Supernova"] = value end,
+                    getFunc = function() return BS.savedVariables["Gravity Crush"] end,
+                    setFunc = function(value) BS.savedVariables["Gravity Crush"] = value end,
+                },
+                [16] = {
+                    type = "checkbox",
+                    name = "Purify",
+                    tooltip = "Disabling this will prevent you from being able to use the Purify synergy, provided by the Cleansing Ritual skill.",
+                    getFunc = function() return BS.savedVariables["Purify"] end,
+                    setFunc = function(value) BS.savedVariables["Purify"] = value end,
+                },
+
+                -- Warden Settings
+                [17] = {
+                    type = "header",
+                    name = "|cFFFACDWarden|r",
+                },
+                [18] = {
+                    type = "checkbox",
+                    name = "Harvest",
+                    tooltip = "Disabling this will prevent you from being able to use the Harvest synergy, provided by the Healing Seed skill.",
+                    getFunc = function() return BS.savedVariables["Harvest"] end,
+                    setFunc = function(value) BS.savedVariables["Harvest"] = value end,
+                },
+                [19] = {
+                    type = "checkbox",
+                    name = "Icy Escape",
+                    tooltip = "Disabling this will prevent you from being able to use the Icy Escape synergy, provided by the Frozen Retreat skill.",
+                    getFunc = function() return BS.savedVariables["Icy Escape"] end,
+                    setFunc = function(value) BS.savedVariables["Icy Escape"] = value end,
+                },
+
+            },
         },
-        {
-            type = "checkbox",
-            name = "Shackle",
-            tooltip = "Disabling this will prevent you from being able to activate the Shackle synergy, provided by the Dragonknight Standard skill.",
-            getFunc = function() return end,
-            setFunc = function(value) end,
+        -- Undaunted Settings
+        [2] = {
+            type = "submenu",
+            name = "|cFFFACDUndaunted Synergies|r",
+            controls = {
+                [1] = {
+                    type = "checkbox",
+                    name = "Altar",
+                    tooltip = "Disabling this will prevent you from being able to use the Blood FUnnel and Blood Feast synergies, provided by the Blood Altar and morphs skills.",
+                    getFunc = function() return BS.savedVariables["Blood Funnel"] end,
+                    setFunc = function(value) BS.savedVariables["Blood Funnel"] = value end,
+                    getFunc = function() return BS.savedVariables["Blood Feast"] end,
+                    setFunc = function(value) BS.savedVariables["Blood Feast"] = value end,
+                },
+                [2] = {
+                    type = "checkbox",
+                    name = "Spiders",
+                    tooltip = "Disabling this will prevent you from being able to use the Spawn Broodlings, Black Widows and Arachnophobia synergies, provided by the Trapping Webs and morphs skills.",
+                    getFunc = function() return BS.savedVariables["Spawn Broodlings"] end,
+                    setFunc = function(value) BS.savedVariables["Spawn Broodlings"] = value end,
+                    getFunc = function() return BS.savedVariables["Black Widows"] end,
+                    setFunc = function(value) BS.savedVariables["Black Widows"] = value end,
+                    getFunc = function() return BS.savedVariables["Arachnophobia"] end,
+                    setFunc = function(value) BS.savedVariables["Arachnophobia"] = value end,
+                },
+                [3] = {
+                    type = "checkbox",
+                    name = "Radiate",
+                    tooltip = "Disabling this will prevent you from being able to use the Radiate synergy, provided by the Inner Fire skill.",
+                    getFunc = function() return BS.savedVariables["Radiate"] end,
+                    setFunc = function(value) BS.savedVariables["Radiate"] = value end,
+                },
+                [4] = {
+                    type = "checkbox",
+                    name = "Bone Shield",
+                    tooltip = "Disabling this will prevent you from being able to use the Bone Wall and Spinal Surge synergies, provided by the Bone Sield and morphs skills.",
+                    getFunc = function() return BS.savedVariables["Bone Wall"] end,
+                    setFunc = function(value) BS.savedVariables["Bone Wall"] = value end,
+                    getFunc = function() return BS.savedVariables["Spinal Surge"] end,
+                    setFunc = function(value) BS.savedVariables["Spinal Surge"] = value end,
+                },
+                [5] = {
+                    type = "checkbox",
+                    name = "Combustion",
+                    tooltip = "Disabling this will prevent you from being able to use the Combustion and Healing Combustion synergies, provided by the Necrotic Orb and morphs skills.",
+                    getFunc = function() return BS.savedVariables["Combustion"] end,
+                    setFunc = function(value) BS.savedVariables["Combustion"] = value end,
+                    getFunc = function() return BS.savedVariables["Healing Combustion"] end,
+                    setFunc = function(value) BS.savedVariables["Healing Combustion"] = value end,
+                },
+            },
+        },
+
+        -- Arena Settings
+        [3] = {
+            type = "submenu",
+            name = "|cFFFACDArena Synergies|r",
+            controls = {
+                [1] = {
+                    type = "checkbox",
+                    name = "Sigil of Defense",
+                    tooltip = "Disabling this will prevent you from being able to use the Sigil of Defense synergy, provided in Maelstrom Arena and Blackrose Prison.",
+                    getFunc = function() return BS.savedVariables["Sigil of Defense"] end,
+                    setFunc = function(value) BS.savedVariables["Sigil of Defense"] = value end,
+                },
+                [2] = {
+                    type = "checkbox",
+                    name = "Sigil of Haste",
+                    tooltip = "Disabling this will prevent you from being able to use the Sigil of Haste synergy, provided in Maelstrom Arena.",
+                    getFunc = function() return BS.savedVariables["Sigil of Haste"] end,
+                    setFunc = function(value) BS.savedVariables["Sigil of Haste"] = value end,
+                },
+                [3] = {
+                    type = "checkbox",
+                    name = "Sigil of Healing",
+                    tooltip = "Disabling this will prevent you from being able to use the Sigil of Healing synergy, provided in Maelstrom Arena and Blackrose Prison.",
+                    getFunc = function() return BS.savedVariables["Sigil of Healing"] end,
+                    setFunc = function(value) BS.savedVariables["Sigil of Healing"] = value end,
+                },
+                [4] = {
+                    type = "checkbox",
+                    name = "Sigil of Power",
+                    tooltip = "Disabling this will prevent you from being able to use the Sigil of Power synergy, provided in Maelstrom Arena.",
+                    getFunc = function() return BS.savedVariables["Sigil of Power"] end,
+                    setFunc = function(value) BS.savedVariables["Sigil of Power"] = value end,
+                },
+                [5] = {
+                    type = "checkbox",
+                    name = "Sigil of Resurrection",
+                    tooltip = "Disabling this will prevent you from being able to use the Sigil of Resurrection synergy, provided in Blackrose Prison.",
+                    getFunc = function() return BS.savedVariables["Sigil of Resurrection"] end,
+                    setFunc = function(value) BS.savedVariables["Sigil of Resurrection"] = value end,
+                },
+                [6] = {
+                    type = "checkbox",
+                    name = "Sigil of Sustain",
+                    tooltip = "Disabling this will prevent you from being able to use the Sigil of Sustain synergy, provided in Blackrose Prison.",
+                    getFunc = function() return BS.savedVariables["Sigil of Sustain"] end,
+                    setFunc = function(value) BS.savedVariables["Sigil of Sustain"] = value end,
+                },
+            },
+        },
+
+        -- Trial Settings
+        [4] = {
+            type = "submenu",
+            name = "|cFFFACDTrial Synergies|r",
+            controls = {
+                -- Craglorn Settings
+                [1] = {
+                    type = "header",
+                    name = "|cFFFACDCraglorn Trials|r",
+                },
+                [2] = {
+                    type = "checkbox",
+                    name = "Destructive Outbreak",
+                    tooltip = "Disabling this will prevent you from being able to use the Destructive Outbreak synergy, provided from the fossilize mechanic during the fight with The Warrior in Hel Ra Citadel.",
+                    getFunc = function() return BS.savedVariables["Destructive Outbreak"] end,
+                    setFunc = function(value) BS.savedVariables["Destructive Outbreak"] = value end,
+                },
+
+                -- Cloudrest Settings
+                [3] = {
+                    type = "header",
+                    name = "|cFFFACDCloudrest|r",
+                },
+                [4] = {
+                    type = "checkbox",
+                    name = "Gateway",
+                    tooltip = "Disabling this will prevent you from being able to use the portal synergy.",
+                    getFunc = function() return BS.savedVariables["Gateway"] end,
+                    setFunc = function(value) BS.savedVariables["Gateway"] = value end,
+                },
+
+                -- Sunspire Settings
+                [5] = {
+                    type = "header",
+                    name = "|cFFFACDSunspire|r",
+                },
+                [6] = {
+                    type = "checkbox",
+                    name = "Time Breach",
+                    tooltip = "Disabling this will prevent you from being able to use the portal synergy.",
+                    getFunc = function() return BS.savedVariables["Time Breach"] end,
+                    setFunc = function(value) BS.savedVariables["Time Breach"] = value end,
+                },
+            },
         },
     }
+
+    LAM:RegisterAddonPanel(BS.name .. "Options", panelData)
+    LAM:RegisterOptionControls(BS.name .. "Options", optionsTable)
 end
