b4da53e1cc9dd1d4d7615a50be31832dbede71ef
BjornTheBurr <lassebjs@gmail.com>
2020-03-26 10:29:43 +0100

    WiP data file

---
+++ BearSynergiesData.lua
@@ -0,0 +1,70 @@
+BearSynergies.Data = {}
+
+local BS = BearSynergies
+local D = BS.Data
+
+D.SynergyIdByName = {
+  -- Dragonknight
+  ["Shackle"] = 
+  ["Ignite"] = 
+
+  -- Necromancer
+  ["Grave Robber"] = 115567,
+  ["Grave Robber"] = 115571,
+  ["Pure Agony"] = 
+
+  -- Nightblade
+  ["Hidden Refresh"] = 
+  ["Soul Leech"] = 
+
+  -- Sorcerer
+  ["Charged Lightning"] = 
+  ["Conduit"] = 
+
+  -- Templar
+  ["Blessed Shards"] = 
+  ["Holy Shards"] = 
+  ["Supernova"] = 
+  ["Gravity Crush"] = 
+  ["Purify"] = 
+
+  -- Warden
+  ["Harvest"] = 
+  ["Icy Escape"] = 
+
+  -- Werewolf
+  ["Feeding Frenzy"] = 
+
+  -- Undaunted
+  ["Blood Funnel"] = 
+  ["Blood Feast"] = 
+  ["Spawn Broodlings"] = 
+  ["Black Widow"] = 
+  ["Arachnophobia"] = 
+  ["Radiate"] = 
+  ["Bone Wall"] = 
+  ["Spinal Surge"] = 
+  ["Combustion"] = 
+  ["Healing Combustion"] = 
+
+  -- Arena
+  ["Sigil of Defense"] = 
+  ["Sigil of Haste"] = 
+  ["Sigil of Healing"] = 
+  ["Sigil of Power"] = 
+  ["Sigil of Resurrection"] = 
+  ["Sigil of Sustain"] = 
+
+  -- Trial
+  ["Destructive Outbreak"] =  -- HRC fossilize break
+  ["Remove Bolt"] =  -- SO and SS remove pin
+  ["Celestial Purge"] =  -- MoL lunar phase
+  ["Levitate"] =  -- HoF 2. boss upstairs
+  ["Power Switch"] =  --HoF 2. boss upstairs button
+  ["Gateway"] =  -- CR portal
+  ["Malevolent Core"] =  -- CR downstairs orbs
+  ["Shed Hoarfrost"] =  -- CR drop hoarfrost
+  ["Welkynar's Light"] =  -- CR spears
+  ["Wind of the Welkynar"] =  -- CR downstairs jump pad
+  ["Time Breach"] =  -- SS portal
+}
